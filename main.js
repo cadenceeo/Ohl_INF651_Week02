a03fcf6e28fdabaff436d21c5adb7cb1256600a1
cadenceeo <ceohl@mail.fhsu.edu>
2022-09-18 18:15:36 -0500

    initial commit

---
+++ main.js
@@ -0,0 +1,28 @@
+//Question 1 and 2
+var myNumber =5;
+
+//Question 3
+var myOtherNumber =5;
+
+//QUestion 4
+var myTotal = myNumber + myOtherNumber;
+
+//Question 5
+const myName = '';
+const myCombo = myNumber + myOtherNumber;
+
+//Question 6
+var myOtherCombo = myNumber + myOtherNumber;
+
+//Question 7
+var myRemainder = myNumber / 5;
+
+//Question 8
+var i =2;
+
+//Question 9
+var int = 4;
+
+//Question 10
+const myBoolean = false;
+
